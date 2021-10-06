e90bc672ae7057bc1384c6a1e5dba9b80be239f6
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-10-06 16:02:13 +0900

    웹 기능 생성

---
+++ src/main/java/com/project/racingarwb/web/MapApiController.java
@@ -0,0 +1,14 @@
+package com.project.racingarwb.web;
+
+import com.project.racingarwb.service.map.MapService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class MapApiController {
+    private final MapService mapService;
+
+    //post
+
+}
---
+++ src/main/java/com/project/racingarwb/web/dto/MapFlagDto.java
@@ -0,0 +1,11 @@
+package com.project.racingarwb.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MapFlagDto {
+    
+
+}
