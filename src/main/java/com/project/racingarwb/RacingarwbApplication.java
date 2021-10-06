f8f1e2578290fa25a20907678dd4856757088040
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-10-06 14:38:48 +0900

    Initial commit

---
+++ src/main/java/com/project/racingarwb/RacingarwbApplication.java
@@ -0,0 +1,13 @@
+package com.project.racingarwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingarwbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacingarwbApplication.class, args);
+	}
+
+}
