f514b36a517978bf8f6e2db3bb04b8eb6e19a4cc
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-10-06 16:06:12 +0900

    Update MapFlagDto.java

--- src/main/java/com/project/racingarwb/web/dto/MapFlagDto.java
+++ src/main/java/com/project/racingarwb/web/dto/MapFlagDto.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class MapFlagDto {
-    
-
+    private final Long id;
+    private final Double latitude;
+    private final Double longitude;
 }
