a1e05dd015ffb84f19a5921d419871254dc0bad7
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-10-06 15:40:06 +0900

    JPA

---
+++ src/main/java/com/project/racingarwb/domain/map/RoadAddress.java
@@ -0,0 +1,21 @@
+package com.project.racingarwb.domain.map;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class RoadAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double latitude;
+    private Double longitude;
+}
---
+++ src/main/java/com/project/racingarwb/domain/map/RoadAddressRepository.java
@@ -0,0 +1,6 @@
+package com.project.racingarwb.domain.map;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoadAddressRepository extends JpaRepository<RoadAddress,Long> {
+}
