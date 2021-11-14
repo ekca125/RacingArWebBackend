3f0d8b9d5d994e029c6ec4d424a12148aa1c8da4
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-11-14 10:29:06 +0900

    API URL 변경

--- src/main/java/com/project/racingarwebbackend/service/map/MapService.java
+++ src/main/java/com/project/racingarwebbackend/service/map/MapService.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class MapService {
-    private final RoadAddressRepository roadAddressRepository;
+    final private RoadAddressRepository roadAddressRepository;
 
-    public MapFlagDto find(Long id) {
+    public MapFlagDto findAddress(Long id) {
         RoadAddress entity = roadAddressRepository
                 .findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 ID가 존재하지 않습니다. id = " + id));
@@ -27,17 +27,12 @@ public class MapService {
         );
     }
 
-    public MapFlagDto sampleFlag() {
-        Long count = roadAddressRepository.count();
-
-        Random random = new Random();
-        random.setSeed(System.currentTimeMillis());
-
-        Long id = random.nextLong();
-        return find(id);
+    public MapFlagDto randomAddress() {
+        long maxSize = roadAddressRepository.count() - 1;
+        return findAddress(ThreadLocalRandom.current().nextLong(maxSize));
     }
 
-    public List<MapFlagDto> mapFlag(Double startLatitude, Double startLongitude, Double endLatitude, Double endLongitude) {
+    public List<MapFlagDto> rangeAddress(Double startLatitude, Double startLongitude, Double endLatitude, Double endLongitude) {
         return roadAddressRepository.queryRange(startLatitude, startLongitude, endLatitude, endLongitude)
                 .stream()
                 .map((entity) -> new MapFlagDto(
@@ -48,8 +43,8 @@ public class MapService {
                 .collect(Collectors.toList());
     }
 
-    public List<MapFlagDto> randomMap(Double startLatitude, Double startLongitude, Double endLatitude, Double endLongitude, long limit) {
-        List<MapFlagDto> mapFlagDtoList = mapFlag(startLatitude, startLongitude, endLatitude, endLongitude);
+    public List<MapFlagDto> rangeRandomAddress(Double startLatitude, Double startLongitude, Double endLatitude, Double endLongitude, long limit) {
+        List<MapFlagDto> mapFlagDtoList = rangeAddress(startLatitude, startLongitude, endLatitude, endLongitude);
         Collections.shuffle(mapFlagDtoList);
 
         return mapFlagDtoList.stream()
--- src/main/java/com/project/racingarwebbackend/web/MapApiController.java
+++ src/main/java/com/project/racingarwebbackend/web/MapApiController.java
@@ -13,31 +13,32 @@ import java.util.Map;
 public class MapApiController {
     private final MapService mapService;
 
-    @GetMapping("/api/v1/address/find/id={id}")
-    public MapFlagDto find(@PathVariable Long id) {
-        return mapService.find(id);
+    @GetMapping("/api/v1/address/id={id}")
+    public MapFlagDto findAddress(@PathVariable Long id) {
+        return mapService.findAddress(id);
     }
 
-    @GetMapping("/api/v1/address/sampleFlag")
-    public MapFlagDto sampleFlag() {
-        return mapService.sampleFlag();
+    @GetMapping("/api/v1/address/random")
+    public MapFlagDto randomAddress() {
+        return mapService.randomAddress();
     }
 
-    @PostMapping("/api/v1/address/rangeMap")
-    public List<MapFlagDto> mapFlag(@RequestBody Map<String, Double> param) {
+    @PostMapping("/api/v1/address/range")
+    public List<MapFlagDto> rangeAddress(@RequestBody Map<String, Double> param) {
         Double startLatitude = param.get("startLatitude");
         Double startLongitude = param.get("startLongitude");
         Double endLatitude = param.get("endLatitude");
         Double endLongitude = param.get("endLongitude");
-        return mapService.mapFlag(startLatitude, startLongitude, endLatitude, endLongitude);
+        return mapService.rangeAddress(startLatitude, startLongitude, endLatitude, endLongitude);
     }
 
-    @PostMapping("/api/v1/address/randomFlag10")
-    public List<MapFlagDto> randomFlag10(@RequestBody Map<String, Double> param) {
+    @PostMapping("/api/v1/address/range-random")
+    public List<MapFlagDto> rangeAddressRandom(@RequestBody Map<String, Double> param) {
         Double startLatitude = param.get("startLatitude");
         Double startLongitude = param.get("startLongitude");
         Double endLatitude = param.get("endLatitude");
         Double endLongitude = param.get("endLongitude");
-        return mapService.randomMap(startLatitude, startLongitude, endLatitude, endLongitude, 10);
+        Long limit = param.get("limit").longValue();
+        return mapService.rangeRandomAddress(startLatitude, startLongitude, endLatitude, endLongitude, limit);
     }
 }
