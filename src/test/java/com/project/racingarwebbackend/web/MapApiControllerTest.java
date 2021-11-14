7ed4252d767f230cb108f4ea8ea363001a986c81
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-11-14 10:36:48 +0900

    Add Test

--- src/test/java/com/project/racingarwebbackend/web/MapApiControllerTest.java
+++ src/test/java/com/project/racingarwebbackend/web/MapApiControllerTest.java
@@ -1,21 +1,44 @@
 package com.project.racingarwebbackend.web;
 
-import com.project.racingarwebbackend.web.dto.MapFlagDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MapApiControllerTest {
+    @LocalServerPort
+    private int port;
 
     @Autowired
-    private MapApiController mapApiController;
+    private TestRestTemplate restTemplate;
 
     @Test
-    public void contextLoads() throws Exception {
-        assertThat(mapApiController).isNotNull();
+    void findAddress() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/v1/address/id=1",
+                String.class)).contains("latitude");
     }
 
+    @Test
+    void randomAddress() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/v1/address/random",
+                String.class)).contains("latitude");
+    }
+
+    @Test
+    void rangeAddress() {
+        Double startLatitude = 35.0979529784;
+        Double startLongitude = 129.0219886069;
+        Double endLatitude = 35.1066801454;
+        Double endLongitude = 129.0290353612;
+
+    }
+
+    @Test
+    void rangeAddressRandom() {
+    }
 }
